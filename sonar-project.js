72c73155bbe607068521c589b66a6059c6c0d1c6
ahmedibrar <ibrarcolt@gmail.com>
2021-02-05 14:42:22 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,6 +1,6 @@
 const sonarqubeScanner = require('sonarqube-scanner');
      sonarqubeScanner({
-       serverUrl: 'http://10.140.100.64:9000',
+       serverUrl: 'http://10.140.100.65:9000',
        options : {
        'sonar.sources': '.',
        //'sonar.inclusions' : '.' // Entry point of your code
